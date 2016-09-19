dbfb6d3417003d6d5eff01362e41ba200b55a97b
Misza <misza@misza.net>
2016-09-19 23:42:12 +0200

    More layout, created control for map display

---
+++ StarsReloaded.Client.View/Controls/Panels/MapPanelControl.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StarsReloaded.View.Controls.Panels
+{
+    /// <summary>
+    /// Interaction logic for MapPanelControl.xaml
+    /// </summary>
+    public partial class MapPanelControl : UserControl
+    {
+        public MapPanelControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
