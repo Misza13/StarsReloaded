e865e6eeb12d218c2688227dcdcb188337765f6c
Misza <misza@misza.net>
2016-09-19 21:50:53 +0200

    Added MVVM Light

--- StarsReloaded.Client.ViewModel/MainWindowViewModel.cs
+++ StarsReloaded.Client.ViewModel/MainWindowViewModel.cs
@@ -1,12 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace StarsReloaded.ViewModel
+﻿namespace StarsReloaded.Client.ViewModel
 {
-    public class MainWindowViewModel
+    using GalaSoft.MvvmLight;
+
+    /// <summary>
+    /// This class contains properties that the main View can data bind to.
+    /// <para>
+    /// Use the <strong>mvvminpc</strong> snippet to add bindable properties to this ViewModel.
+    /// </para>
+    /// <para>
+    /// You can also use Blend to data bind with the tool's support.
+    /// </para>
+    /// <para>
+    /// See http://www.galasoft.ch/mvvm
+    /// </para>
+    /// </summary>
+    public class MainWindowViewModel : ViewModelBase
     {
+        /// <summary>
+        /// Initializes a new instance of the MainViewModel class.
+        /// </summary>
+        public MainWindowViewModel()
+        {
+            if (IsInDesignMode)
+            {
+                Test = "Design";
+            }
+            else
+            {
+                Test = "Real";
+            }
+        }
+
+        public string Test { get; set; }
     }
 }
---
+++ StarsReloaded.Client.ViewModel/ViewModelLocator.cs
@@ -0,0 +1,41 @@
+namespace StarsReloaded.Client.ViewModel
+{
+    using GalaSoft.MvvmLight.Ioc;
+    using Microsoft.Practices.ServiceLocation;
+
+    /// <summary>
+    /// This class contains static references to all the view models in the
+    /// application and provides an entry point for the bindings.
+    /// </summary>
+    public class ViewModelLocator
+    {
+        /// <summary>
+        /// Initializes a new instance of the ViewModelLocator class.
+        /// </summary>
+        public ViewModelLocator()
+        {
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+
+            ////if (ViewModelBase.IsInDesignModeStatic)
+            ////{
+            ////    // Create design time view services and models
+            ////    SimpleIoc.Default.Register<IDataService, DesignDataService>();
+            ////}
+            ////else
+            ////{
+            ////    // Create run time view services and models
+            ////    SimpleIoc.Default.Register<IDataService, DataService>();
+            ////}
+
+            SimpleIoc.Default.Register<MainWindowViewModel>();
+        }
+
+        public MainWindowViewModel MainWindow
+        {
+            get
+            {
+                return ServiceLocator.Current.GetInstance<MainWindowViewModel>();
+            }
+        }
+    }
+}
