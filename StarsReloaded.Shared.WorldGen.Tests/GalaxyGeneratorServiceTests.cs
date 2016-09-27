5dffe2e12d6d7103699b1fa55db6c460372398e1
Misza <misza@misza.net>
2016-09-27 22:45:40 +0200

    Added (empty) project for worldgen unit tests

---
+++ StarsReloaded.Shared.WorldGen.Tests/GalaxyGeneratorServiceTests.cs
@@ -0,0 +1,18 @@
+﻿namespace StarsReloaded.Shared.WorldGen.Tests
+{
+    using NUnit.Framework;
+    using StarsReloaded.Shared.WorldGen.Services;
+
+    [TestFixture]
+    public class GalaxyGeneratorServiceTests
+    {
+        ////System Under Test
+        private IGalaxyGeneratorService _galaxyGeneratorService;
+
+        [SetUp]
+        public void Setup()
+        {
+            _galaxyGeneratorService = new GalaxyGeneratorService();
+        }
+    }
+}
