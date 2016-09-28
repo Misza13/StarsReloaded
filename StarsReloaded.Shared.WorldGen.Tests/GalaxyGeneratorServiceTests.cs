e9336c1f55200925b886ea5960d0121e498a748b
Misza <misza@misza.net>
2016-09-28 23:02:36 +0200

    Unit test for Galaxy generator

--- StarsReloaded.Shared.WorldGen.Tests/GalaxyGeneratorServiceTests.cs
+++ StarsReloaded.Shared.WorldGen.Tests/GalaxyGeneratorServiceTests.cs
@@ -1,6 +1,8 @@
 ﻿namespace StarsReloaded.Shared.WorldGen.Tests
 {
     using NUnit.Framework;
+    using StarsReloaded.Shared.WorldGen.Helpers;
+    using StarsReloaded.Shared.WorldGen.Meta;
     using StarsReloaded.Shared.WorldGen.Services;
 
     [TestFixture]
@@ -13,6 +15,30 @@
         public void Setup()
         {
             _galaxyGeneratorService = new GalaxyGeneratorService();
+        }
+
+        [Test]
+        [Combinatorial]
+        public void GenerateUniformEnums_ShouldCreateCorrectGalaxy(
+            [Values(GalaxySize.Tiny, GalaxySize.Small, GalaxySize.Medium, GalaxySize.Large, GalaxySize.Huge)] GalaxySize size,
+            [Values(GalaxyDensity.Sparse, GalaxyDensity.Normal, GalaxyDensity.Dense, GalaxyDensity.Packed)] GalaxyDensity density)
+        {
+            ////Arrange
+            var edge = size.GetAttributeOfType<GalaxyEdgeAttribute>().Edge;
+            var num = density.GetAttributeOfType<BasePlanetCountAttribute>().Num * edge * edge / 160000;
+
+            ////Act
+            var galaxy = _galaxyGeneratorService.GenerateUniform(size, density);
+
+            ////Assert
+            Assert.That(galaxy.Width, Is.EqualTo(edge));
+            Assert.That(galaxy.Height, Is.EqualTo(edge));
+            Assert.That(galaxy.Planets.Count, Is.EqualTo(num));
+            foreach (var planet in galaxy.Planets)
+            {
+                Assert.That(planet.X, Is.InRange(0, edge));
+                Assert.That(planet.Y, Is.InRange(0, edge));
+            }
         }
     }
 }
