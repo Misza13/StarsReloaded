94f202ee0c1525abd7fd751b138be9f2984e974b
Misza <misza@misza.net>
2016-10-20 23:52:21 +0200

    Validation of hab range

--- StarsReloaded.Shared.Model/HabitationParameter.cs
+++ StarsReloaded.Shared.Model/HabitationParameter.cs
@@ -1,9 +1,16 @@
 ﻿namespace StarsReloaded.Shared.Model
 {
+    using System;
+
     public class HabitationParameter
     {
         public HabitationParameter(int clicks)
         {
+            if (clicks < -50 || clicks > 50)
+            {
+                throw new ArgumentException("Habitation value must be between -50 and +50 \"clicks\".");
+            }
+
             this.Clicks = clicks;
         }
 
