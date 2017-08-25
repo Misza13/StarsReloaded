c9d33122a2141976c9c5cdf99345687e9e78f591
Misza <misza@misza.net>
2017-08-25 19:39:39 +0200

    Rewrite BaseViewModel's property resolution to keep dependency map in a static field and only resolve the dependencies once during the lifetime of an application.

--- StarsReloaded.Client.ViewModel/BaseViewModel.cs
+++ StarsReloaded.Client.ViewModel/BaseViewModel.cs
@@ -1,6 +1,7 @@
 ﻿namespace StarsReloaded.Client.ViewModel
 {
     using System;
+    using System.Collections.Generic;
     using System.Reflection;
     using System.Runtime.CompilerServices;
 
@@ -10,23 +11,74 @@
 
     public class BaseViewModel : ViewModelBase
     {
+        private static readonly Dictionary<Type, Dictionary<string, List<string>>> PropertyDependencies =
+            new Dictionary<Type, Dictionary<string, List<string>>>();
+
         public override void RaisePropertyChanged([CallerMemberName] string propertyName = null)
         {
             base.RaisePropertyChanged(propertyName);
 
-            foreach (var property in this.GetType().GetProperties())
+            foreach (var property in GetDependentProperties(this.GetType(), propertyName))
             {
-                foreach (var baseProp in property.GetCustomAttributes(typeof(DependsUponAttribute)))
-                {
-                    var dependentUpon = (baseProp as DependsUponAttribute)?.PropertyName;
-                    if (dependentUpon == propertyName)
-                    {
 #if DEBUG
-                        Console.WriteLine($"{propertyName} -> {property.Name}");
+                Console.WriteLine($"{propertyName} -> {property}");
 #endif
-                        this.RaisePropertyChanged(property.Name);
+                this.RaisePropertyChanged(property);
+            }
+        }
+
+        private static IEnumerable<string> GetDependentProperties(Type type, string propName)
+        {
+            lock (PropertyDependencies)
+            {
+                if (!PropertyDependencies.ContainsKey(type))
+                {
+                    PropertyDependencies[type] = ResolvePropertyDependencies(type);
+                }
+
+                var typeMap = PropertyDependencies[type];
+                if (!typeMap.ContainsKey(propName))
+                {
+                    return new string[0];
+                }
+
+                return PropertyDependencies[type][propName];
+            }
+        }
+
+        private static Dictionary<string, List<string>> ResolvePropertyDependencies(Type type)
+        {
+#if DEBUG
+            Console.WriteLine($"Resolving property dependencies for {type.FullName}");
+#endif
+
+            var map = new Dictionary<string, List<string>>();
+
+            foreach (var property in type.GetProperties())
+            {
+                foreach (var dependency in property.GetCustomAttributes(typeof(DependsUponAttribute)))
+                {
+                    var dependsUpon = (dependency as DependsUponAttribute)?.PropertyName;
+                    if (dependsUpon != null && dependsUpon != property.Name)
+                    {
+                        RegisterDependency(map, dependsUpon, property.Name);
                     }
                 }
+            }
+
+            return map;
+        }
+
+        private static void RegisterDependency(IDictionary<string, List<string>> map, string fromProp, string toProp)
+        {
+            if (!map.ContainsKey(fromProp))
+            {
+                map[fromProp] = new List<string>();
+            }
+
+            if (!map[fromProp].Contains(toProp))
+            {
+                map[fromProp].Add(toProp);
             }
         }
     }
